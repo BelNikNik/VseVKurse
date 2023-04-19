0f28634883399c1e58fe063a26834cd717bbdc2e
BelNikNik <36932712+BelNikNik@users.noreply.github.com>
2023-04-19 07:52:32 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ const iconMenu = document.querySelector('.header__menu-icon');
 const menuBody = document.querySelector('.header__list');
 if (iconMenu) {
 	iconMenu.addEventListener("click", function (e) {
-		/* document.body.classList.toggle('_lock') */
+		document.body.classList.toggle('_lock')
 		iconMenu.classList.toggle('_active');
 		menuBody.classList.toggle('_active');
 	});
